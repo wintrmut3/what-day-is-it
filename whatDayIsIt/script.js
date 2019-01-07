f24b20b0a39efca6bef05da8f4c8d2cf72d98db9
wintrmut3 <36431739+wintrmut3@users.noreply.github.com>
2019-01-07 15:57:21 -0500

    switched back

--- whatDayIsIt/script.js
+++ whatDayIsIt/script.js
@@ -34,5 +34,5 @@ for (var i=0; i < nDays.length; i++){
 
 dateDiff -= passedNDays;
 var datebox = document.getElementById('dayNum');
-var numMap = ["Two", "One"]
+var numMap = ["One", "Two"]
 datebox.innerHTML = numMap[dateDiff%2];
